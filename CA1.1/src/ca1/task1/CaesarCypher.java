cbfe80ed5e25bd6130a87d337cc2e44e3b1eb727
adampabr <adampb23@gmail.com>
2024-09-18 17:06:31 +0930

    Initial code writing for task 1. Code does not currently run.

---
+++ CA1.1/src/ca1/task1/CaesarCypher.java
@@ -0,0 +1,18 @@
+package ca1.task1;
+
+public class CaesarCypher {
+
+	public char[] encode(char[] input, int offset) {
+		char[] result = new char[input.length];
+		
+		for (int i = 0; i < input.length; i++) {
+			char c = input[i];
+			
+			if(Character)
+		}
+	}
+	
+	public char[] decode(char[] input, int offset) {
+		return null;
+	}
+}
