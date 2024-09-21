b782522849949d8d0008de8dfacfe89faeb7aad7
adampabr <adampb23@gmail.com>
2024-09-21 11:03:03 +0930

    Beginning of coding task 4 for car.java and carProcessing.java. Not yet operational.

---
+++ CA1.1/src/ca1/task4/Car.java
@@ -0,0 +1,37 @@
+package ca1.task4;
+
+import java.util.Objects;
+
+public class Car {
+	private String make;
+	private String model;
+	private String color;
+	
+	
+	// Creating a constructor to access the car details	
+	public Car(String make, String model, String color) {
+		this.make = make;
+		this.model = model;
+		this.color = color;
+	}
+	
+	@Override
+	public boolean equals (Object o) {
+		if (this == o) return true;
+		if (getClass() != o.getClass()) return false;
+		Car car = (Car) o;
+		return Objects.equals(model, car.model) && Objects.equals(color, car.color)
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(model, color); 
+	}
+	
+	@Override
+	public String toString( ) {
+		return make + ", " + model + ", " + color;
+	}
+	
+	
+}
